bde6b1d4fe5240c4a779827ed1284cdddde8c2ae
hzmsh <35788891+hzmsh@users.noreply.github.com>
2020-07-04 23:47:57 -0500

    updates

---
+++ instaloader.py
@@ -0,0 +1,2 @@
+# py -3 -m pip install matplotlib
+# py -3 -m instaloader profile[]
---
+++ sweetviz.py
@@ -0,0 +1,5 @@
+# pip install sweetviz
+import sweetviz as sv
+my_report = sv.analyze(df)
+
+myreport.show_html()
